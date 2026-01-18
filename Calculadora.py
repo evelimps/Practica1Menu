c4c2f1a1483b41ff59b32561b44e77ffcca9fc34
Lesly Maybet Perez Gomez <maybetplesly@gmail.com>
2026-01-17 20:11:12 -0600

    Update Calculadora.py
    agregue producto y division

--- Calculadora.py
+++ Calculadora.py
@@ -21,15 +21,43 @@ def suma_n_numeros():
             suma += float(input(f"Ingrese el número {i}: "))
         print(f"\nLa suma total es: {suma}")
     except ValueError: print("Error: Entrada inválida.")
+def producto_n_numeros():
+    print("\n--- PRODUCTO DE 'N' NÚMEROS ---")
+    try:
+        n = int(input("¿Cuántos números desea multiplicar?: "))
+        producto = 1
+        for i in range(1, n + 1):
+            producto *= float(input(f"Ingrese el número {i}: "))
+        print(f"\nEl resultado del producto es: {producto}")
+    except ValueError: print("Error: Entrada inválida.")
+
+def division():
+    try:
+        n1 = float(input("Ingrese el dividendo: "))
+        n2 = float(input("Ingrese el divisor: "))
+        if n2 == 0:
+            print("Error: No se puede dividir entre cero.")
+        else:
+            print(f"Resultado: {n1 / n2}")
+    except ValueError: print("Error: Entrada inválida.")
 def main():
     while True:
         mostrar_menu()
         opcion = input("\nSeleccione una opción (1-9): ")
-        if opcion == '1':
-            suma_n_numeros()
+        
+        if opcion == '1': suma_n_numeros()
+        elif opcion == '2': producto_n_numeros()
+        elif opcion == '3': division()
+        elif opcion == '4': calcular_factorial()
+        elif opcion == '5': tablas_multiplicar()
+        elif opcion == '6': cuadrado_cubo()
+        elif opcion == '7': promedio_serie()
+        elif opcion == '8': maximo_minimo()
         elif opcion == '9':
-            print("Saliendo...")
+            print("Saliendo del programa...")
             break
+        else:
+            print("Opción no válida. Intente de nuevo.")
 
 if __name__ == "__main__":
     main()
